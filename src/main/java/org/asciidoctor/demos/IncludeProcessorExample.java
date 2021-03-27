22635aec5a7a8526a314751b37419656cf37db97
Abel Salgado Romero <abelromero@gmail.com>
2021-03-27 12:37:00 +0100

    Add CodeCommentIncludeProcessor

--- src/main/java/org/asciidoctor/demos/IncludeProcessorExample.java
+++ src/main/java/org/asciidoctor/demos/IncludeProcessorExample.java
@@ -1,37 +1,34 @@
 package org.asciidoctor.demos;
 
 import org.asciidoctor.*;
+import org.asciidoctor.demos.extensions.CodeCommentIncludeProcessor;
 
 import java.io.File;
 
-public class AsciidoctorRunner {
+public class IncludeProcessorExample {
 
     public static final String SRC_PATH = "src/asciidoc/";
 
     public static void main(String[] args) {
         Asciidoctor asciidoctor = Asciidoctor.Factory.create();
 
+        asciidoctor.javaExtensionRegistry()
+            .includeProcessor(CodeCommentIncludeProcessor.class);
+
         AttributesBuilder attributes = AttributesBuilder.attributes();
         attributes.tableOfContents(true);
         attributes.tableOfContents(Placement.LEFT);
-//        attributes.icons("font");
+        attributes.icons("font");
 
-        OptionsBuilder options = OptionsBuilder.options();
-        options.safe(SafeMode.UNSAFE);
-        options.mkDirs(true);
-        options.attributes(attributes);
-        options.toDir(new File("build"));
+        var options = new Options();
+        options.setInPlace(true);
+        options.setSafe(SafeMode.SAFE);
+        options.setToDir("." + "/build");
+        options.setMkDirs(true);
 
-        asciidoctor.convertFile(file("sample.adoc"), options);
-
-        options.backend("pdf");
-        setSourceHighlighter("pdf", attributes);
-        asciidoctor.convertFile(file("sample.adoc"), options);
-
-        options.backend("html5");
+        options.setBackend("html5");
         setSourceHighlighter("html5", attributes);
-        asciidoctor.convertFile(file("sample.adoc"), options);
-        // asciidoctor.convertFile(file("example-manual.adoc"), options);
+        asciidoctor.convertFile(file("custom-include-processor.adoc"), options);
     }
 
 
